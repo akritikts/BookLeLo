041b11f678cd49795e7411c6944e9698a2c526c1
akriti <akritikts@gmail.com>
2016-09-03 17:25:27 +0530

    Packages updated

--- app/src/main/java/in/silive/booklelo/Activities/MainActivity.java
+++ app/src/main/java/in/silive/booklelo/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package in.silive.booklelo;
+package in.silive.booklelo.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import in.silive.booklelo.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/in/silive/booklelo/Activities/SplashScreen.java
@@ -0,0 +1,18 @@
+package in.silive.booklelo.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import in.silive.booklelo.R;
+
+/**
+ * Created by akriti on 3/9/16.
+ */
+public class SplashScreen extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}

40ec97044acd83fba59006bee97a8ddca9f58fe9
akriti <akritikts@gmail.com>
2016-09-11 10:15:23 +0530

    flow working

--- app/src/main/java/in/silive/booklelo/Activities/Login.java
+++ app/src/main/java/in/silive/booklelo/Activities/Login.java
@@ -15,37 +15,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //  Initialize SharedPreferences
-                SharedPreferences getPrefs = PreferenceManager
-                        .getDefaultSharedPreferences(getBaseContext());
 
-                //  Create a new boolean and preference and set it to true
-                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
-
-                //  If the activity has never started before...
-                if (isFirstStart) {
-
-                    //  Launch app intro
-                    Intent i = new Intent(Login.this, SlideIntro.class);
-                    startActivity(i);
-                    //finish();
-
-                    //  Make a new preferences editor
-                    SharedPreferences.Editor e = getPrefs.edit();
-
-                    //  Edit preference to make it false because we don't want this to run again
-                    e.putBoolean("firstStart", false);
-
-                    //  Apply changes
-                    e.apply();
-                }
-            }
-        });
-        // Start the thread
-        t.start();
-        //finish();
     }
 }
--- app/src/main/java/in/silive/booklelo/Activities/MainActivity.java
+++ app/src/main/java/in/silive/booklelo/Activities/MainActivity.java
@@ -1,58 +1,59 @@
 package in.silive.booklelo.Activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.LinearLayout;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import in.silive.booklelo.Models.SlideIntro;
 import in.silive.booklelo.R;
-import yalantis.com.sidemenu.interfaces.Resourceble;
-import yalantis.com.sidemenu.model.SlideMenuItem;
-import yalantis.com.sidemenu.util.ViewAnimator;
 
-public class MainActivity extends AppCompatActivity implements ActionBarDrawerToggle.DelegateProvider {
-    DrawerLayout drawerLayout;
-    Fragment container;
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        ViewAnimator viewAnimator = new ViewAnimator<>(ActionBarActivity.this,
-                new ArrayList<Resourceble>(),
-                (LinearLayout) findViewById(R.id.left_drawer),
-                container, drawerLayout);
-        private List<SlideMenuItem> list = new ArrayList<>();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
 
-        SlideMenuItem menuItem0 = new SlideMenuItem("Close", R.mipmap.ic_launcher);
-        list.add(menuItem0);
-        SlideMenuItem menuItem = new SlideMenuItem("Building", R.mipmap.ic_launcher);
-        list.add(menuItem);
-        SlideMenuItem menuItem2 = new SlideMenuItem("Book", R.mipmap.ic_launcher);
-        list.add(menuItem2);
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
 
-        viewAnimator = new ViewAnimator<>(this, list, container, drawerLayout, this);
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    Intent i = new Intent(MainActivity.this, SlideIntro.class);
+                    startActivity(i);
+                    //finish();
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+        // Start the thread
+        t.start();
+        //finish();
+
+
     }
-    @Override
-    public void onDrawerSlide(View drawerView, float slideOffset) {
-        super.onDrawerSlide(drawerView, slideOffset);
-        if (slideOffset > 0.6 && viewAnimator.getLinearLayout().getChildCount() == 0)
-            viewAnimator.showMenuContent();
-    }
-    @Override
-    public void onDrawerClosed(View view) {
-        super.onDrawerClosed(view);
-        viewAnimator.getLinearLayout().removeAllViews();
-        viewAnimator.getLinearLayout().invalidate();
-    }
+
+
+
 
 
 }
